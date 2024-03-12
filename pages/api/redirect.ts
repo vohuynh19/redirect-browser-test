765516afefdb5106cad3363f24b48aac1a913602
VoHuynh <huynh.vo@unifiedpost.com>
2024-03-12 15:45:15 +0700

    fix1

--- pages/api/redirect.ts
+++ pages/api/redirect.ts
@@ -9,5 +9,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.redirect("https://open.spotify.com");
+  res.redirect(
+    "https://test.links.banqup.com/enrol?code%3Dd971aee631b1f11da5e51265a75fd0c6dca2bb1f28298b39f9fccb7224d24a98%26apn%3Dcom.unifiedpost.myid.test%26ibi%3Dcom.unifiedpost-payments.myid.test%26isi%3D1594778737%26fr%3D1"
+  );
 }
