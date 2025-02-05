708d91fa38a6c6377722f1fd5744b61fdf3a94df
VoHuynh <huynh.vo@unifiedpost.com>
2025-02-05 13:55:35 +0700

    feat: update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,10 @@ export default function Document() {
           TryRedirect
         </title>
         <meta
+          name="title"
+          content="Browser Redirect Simulation Tool | Test Browser Redirects | TryRedirect"
+        />
+        <meta
           name="description"
           content="Free online web redirect simulation tool. Test and analyze different browser redirect methods including client-side, server-side, and delayed redirects in a safe environment."
         />
@@ -27,8 +31,8 @@ export default function Document() {
           content="Free online web redirect simulation tool. Test and analyze different browser redirect methods including client-side, server-side, and delayed redirects in a safe environment."
         />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/preview.svg" />
-        <meta property="og:site_name" content="TryRedirect" />
+        <meta property="og:image" content="/preview.png" />
+        <meta property="og:site_name" content="Try Redirect" />
         <meta property="og:locale" content="en_US" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta
@@ -39,9 +43,9 @@ export default function Document() {
           name="twitter:description"
           content="Free online tool to test and analyze different browser redirect methods in a safe environment."
         />
-        <meta name="twitter:image" content="/preview.svg" />
+        <meta name="twitter:image" content="/preview.png" />
         <link rel="canonical" href="https://tryredirect.net" />
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
